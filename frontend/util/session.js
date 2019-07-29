45d99b7642713a63ea57c4063640e6b324fc503e
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-07-29 19:15:27 -0400

    backend user auth/react rendering

--- frontend/util/session.js
+++ frontend/util/session.js
@@ -1,22 +1,22 @@
-const postUser = user => (
-    $.ajax({
-        url: '/api/users',
-        method: "POST",
-        data: { user }
-    })
-)
+// const postUser = user => (
+//     $.ajax({
+//         url: '/api/users',
+//         method: "POST",
+//         data: { user }
+//     })
+// )
 
-const postSession = user => (
-    $.ajax({
-        url: `/api/session`,
-        method: "post",
-        data: { user }
-    })
-)
+// const postSession = user => (
+//     $.ajax({
+//         url: `/api/session`,
+//         method: "post",
+//         data: { user }
+//     })
+// )
 
-const postSession = () => (
-    $.ajax({
-        url: `/api/session`,
-        method: "DELETE",
-    })
-)
+// const postSession = () => (
+//     $.ajax({
+//         url: `/api/session`,
+//         method: "DELETE",
+//     })
+// )
--- frontend/yuup.jsx
+++ frontend/yuup.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import store from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore();
+    // const store = configureStore();
     ReactDOM.render(<h1>Yuup</h1>, document.getElementById('root'))
 })
